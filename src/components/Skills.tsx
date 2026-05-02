df0ba2478249c13399d6c2b36a74607e084739eb
Viren Patadiya <102893049+VIREN2779@users.noreply.github.com>
2026-05-02 05:50:25 +0000

    feat(Skills.tsx): initialize Skills component to showcase technical skills

---
+++ src/components/Skills.tsx
@@ -0,0 +1,14 @@
+export default function Skills() {
+    return (
+        <div className="skills">
+            <h2>Skills</h2>
+            <ul>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>JavaScript/TypeScript</li>
+                <li>CSS</li>
+                <li>HTML</li>
+            </ul>
+        </div>
+    );
+}
